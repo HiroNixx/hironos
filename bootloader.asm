e0317dbd23664699ac8223e5574eda5d00062b2d
HiroNixx <pettit.matt@gmail.com>
2020-10-09 23:20:35 +0200

    Revert "Added diskread and reformatted directory structure"
    This reverts commit ddaefce00f6049f6fbd1e6e481794d69d3a5ef0e.

--- bootloader.asm
+++ bootloader.asm
@@ -1,20 +1,15 @@
 
 [org 0x7c00]
 
-mov [BOOT_DISK], dl
-
 mov bp, 0x7c00
 mov sp, bp
 
 mov bx, TestString
 call PrintString
 
-call ReadDisk
-
 jmp $
 
 %include "print.asm"
-%include "diskread.asm"
 
 times 510-($-$$) db 0
 
